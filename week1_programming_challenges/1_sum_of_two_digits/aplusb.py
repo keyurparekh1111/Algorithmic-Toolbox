807abcb1068c87bb07af26175e5f01b3d0f492fe
keyurparekh1111 <keyurparekh1111@gmail.com>
2020-05-14 20:18:19 +0530

    week 1

---
+++ week1_programming_challenges/1_sum_of_two_digits/aplusb.py
@@ -0,0 +1,6 @@
+def aplusb(first_digit,second_digit):
+    return first_digit + second_digit;
+
+if __name__ == '__main__':
+    a,b=map(int,input().split())
+    print(aplusb(a,b))
---
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -0,0 +1,20 @@
+# python3
+
+
+def max_pairwise_product(numbers):
+    # n = len(numbers)
+    # max_product = 0
+    # for first in range(n):
+    #     for second in range(first + 1, n):
+    #         max_product = max(max_product,
+    #             numbers[first] * numbers[second])
+    m = max(numbers)
+    numbers.remove(m)
+    n = max(numbers)
+    max_product = m*n
+    return max_product
+
+if __name__ == '__main__':
+    input_n = int(input())
+    input_numbers = [int(x) for x in input().split()]
+    print(max_pairwise_product(input_numbers))
